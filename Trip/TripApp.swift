af2d259ff79b2d59c6729b418d1105c0ee517826
MAHESHWARAN <maheshwaran@eam360.com>
2024-04-21 10:21:54 +0530

    Initial Commit

---
+++ Trip/TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripApp.swift
+//  Trip
+//
+//  Created by MAHESHWARAN on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
