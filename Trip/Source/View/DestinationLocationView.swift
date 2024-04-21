073227838d0f32333d6d93aa1acd6af196d35b0b
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-21 10:34:34 +0530

    Source  - First Commit

---
+++ Trip/Source/View/DestinationLocationView.swift
@@ -0,0 +1,21 @@
+//
+//  DestinationLocationView.swift
+//  Trip
+//
+//  Created by MAHESHWARAN on 21/04/24.
+//
+
+import SwiftUI
+
+struct DestinationLocationView: View {
+  
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+// MARK: - Preview
+
+#Preview {
+  DestinationLocationView()
+}
---
+++ Trip/Source/View/TripMapView.swift
@@ -0,0 +1,21 @@
+//
+//  TripMapView.swift
+//  Trip
+//
+//  Created by MAHESHWARAN on 21/04/24.
+//
+
+import SwiftUI
+
+struct TripMapView: View {
+  
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+// MARK: - Preview
+
+#Preview {
+  TripMapView()
+}
