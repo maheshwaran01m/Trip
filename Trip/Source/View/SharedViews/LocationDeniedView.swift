43d0d0c233568d7776f1a5ab2004f5490e14505c
MAHESHWARAN <maheshwaran@eam360.com>
2024-04-23 06:30:04 +0530

    Source  - Updated LocationDeniedView

--- Trip/Source/View/SharedViews/LocationDeniedView.swift
+++ Trip/Source/View/SharedViews/LocationDeniedView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct LocationDeniedView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  
+  var body: some View {
+    ContentUnavailableView(label: {
+      Label("Location Services", systemImage: "location.circle")
+    }, description: {
+      
+      Text("""
+           1. Tap the button below and go to "Privacy and Security"
+           2. Tap on "Location Services"
+           3. Locate the "Trip" app and tap on it,
+           4. Change the settings to "While using the App"
+           """)
+      .multilineTextAlignment(.leading)
+      
+    }, actions: {
+      if let url = URL(string: UIApplication.openSettingsURLString) {
+        
+        Link(destination: url) {
+          Text("Open Settings")
+        }
+        .buttonStyle(.borderedProminent)
+      }
+    })
+  }
 }
 
 #Preview {
-    LocationDeniedView()
+  LocationDeniedView()
 }
